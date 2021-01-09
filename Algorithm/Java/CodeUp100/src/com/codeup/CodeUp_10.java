ec482efa04dc5bdbec6f67f17dcdc44bf6485036
wintryjay <nhj3900@gmail.com>
2021-01-09 11:52:22 +0900

    CodeUP100

---
+++ Algorithm/Java/CodeUp100/src/com/codeup/CodeUp_10.java
@@ -0,0 +1,13 @@
+package com.codeup;
+
+
+import java.util.Scanner;
+
+public class CodeUp_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        System.out.println(n);
+    }
+}
