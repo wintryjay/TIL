0e315a1114898a6bbebd704ba18af4fc6891be2c
wintryjay <nhj3900@gmail.com>
2021-01-09 11:54:09 +0900

    CodeUP100

---
+++ Algorithm/Java/CodeUp100/src/com/codeup/CodeUp_15.java
@@ -0,0 +1,12 @@
+package com.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp_15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        String b = String.format("%.2f", a);
+        System.out.println(b);
+    }
+}
